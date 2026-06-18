c31815a6e5cb1292987a387c19a24b7ee1a0886c
matter172 <matter172@service.zaman.my>
2026-06-19 02:15:42 +0800

    Add files via upload

---
+++ 01-rooted-remove-software.sh
@@ -0,0 +1,24 @@
+sudo dnf remove -y \
+	libreoffice-core.x86_64 \
+	gnome-tour.x86_64 \
+	firefox.x86_64 \
+	gnome-calendar.x86_64 \
+	gnome-boxes.x86_64 \
+	gnome-contacts.x86_64 \
+	mediawriter.x86_64 \
+	gnome-weather.noarch \
+	gnome-maps.x86_64 \
+	simple-scan.x86_64 \
+	yelp.x86_64 \
+	gnome-connections.x86_64 \
+	malcontent-control.x86_64 \
+	gnome-abrt.x86_64 \
+	gnome-font-viewer.x86_64 \
+	papers.x86_64 \
+	gnome-calculator.x86_64 \
+	gnome-clocks.x86_64 \
+	showtime.noarch \
+	gnome-characters.x86_64 \
+	loupe.x86_64 \
+	decibels.noarch \
+	gnome-system-monitor.x86_64
---
+++ 03-rooted-add-software.sh
@@ -0,0 +1,5 @@
+curl -fsSL https://raw.githubusercontent.com/matter172/unofficial-proton-pass-rpm/refs/heads/main/setup.sh | sudo bash
+
+sudo dnf install -y \
+	proton-pass \
+	pipx
