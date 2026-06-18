86a9d1000285c778743541c3d344130f8e4dd677
matter172 <matter172@service.zaman.my>
2026-06-19 02:27:41 +0800

    Refactor setup script to use curl for scripts

--- 00-setup.sh
+++ 00-setup.sh
@@ -1,4 +1,8 @@
-sudo bash 01-rooted-remove-software.sh
-sudo bash 02-rooted-update-fedora.sh
-sudo bash 03-rooted-add-software.sh
-bash 04-non-rooted-add-software.sh
+#!/bin/bash
+
+BASE_URL="https://raw.githubusercontent.com/matter172/dotfiles/main"
+
+sudo bash <(curl -fsSL "$BASE_URL/01-rooted-remove-software.sh")
+sudo bash <(curl -fsSL "$BASE_URL/02-rooted-update-fedora.sh")
+sudo bash <(curl -fsSL "$BASE_URL/03-rooted-add-software.sh")
+bash <(curl -fsSL "$BASE_URL/04-non-rooted-add-software.sh")
