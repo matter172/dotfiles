f841332dd19732daab7fd7be66642f1a8d0919a9
matter172 <matter172@service.zaman.my>
2026-06-19 02:21:10 +0800

    Change script to add software for non-rooted setup

--- 00-setup.sh
+++ 00-setup.sh
@@ -1,4 +1,4 @@
 sudo bash 01-rooted-remove-software.sh
 sudo bash 02-rooted-update-fedora.sh
 sudo bash 03-rooted-add-software.sh
-bash 04-rooted-remove-software.sh
+bash 04-non-rooted-add-software.sh
