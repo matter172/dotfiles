93d991851743ddbbb659ca9abdffde31a1100766
matter172 <matter172@service.zaman.my>
2026-06-19 02:32:08 +0800

    Update 00-setup.sh

--- 00-setup.sh
+++ 00-setup.sh
@@ -2,7 +2,14 @@
 
 BASE_URL="https://raw.githubusercontent.com/matter172/dotfiles/main"
 
-curl -fsSL "$BASE_URL/01-rooted-remove-software.sh" | sudo bash
-curl -fsSL "$BASE_URL/02-rooted-update-fedora.sh"   | sudo bash
-curl -fsSL "$BASE_URL/03-rooted-add-software.sh"    | sudo bash
-curl -fsSL "$BASE_URL/04-non-rooted-add-software.sh" | bash
+curl -fsSL "$BASE_URL/01-rooted-remove-software.sh" -o /tmp/01.sh
+curl -fsSL "$BASE_URL/02-rooted-update-fedora.sh"   -o /tmp/02.sh
+curl -fsSL "$BASE_URL/03-rooted-add-software.sh"    -o /tmp/03.sh
+curl -fsSL "$BASE_URL/04-non-rooted-add-software.sh" -o /tmp/04.sh
+
+sudo bash /tmp/01.sh
+sudo bash /tmp/02.sh
+sudo bash /tmp/03.sh
+bash /tmp/04.sh
+
+rm /tmp/01.sh /tmp/02.sh /tmp/03.sh /tmp/04.sh
