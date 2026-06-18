4fb97cbf0d66beda391c2ffac72c3174230a4630
matter172 <matter172@service.zaman.my>
2026-06-19 02:40:37 +0800

    Refactor script URL and add cache busting
    Updated BASE_URL to remove 'refs/heads' and added cache busting to script downloads.

--- 00-setup.sh
+++ 00-setup.sh
@@ -1,8 +1,9 @@
 #!/bin/bash
 
-BASE_URL="https://raw.githubusercontent.com/matter172/dotfiles/refs/heads/main"
+BASE_URL="https://raw.githubusercontent.com/matter172/dotfiles/main"
+BUST="?$(date +%s)"
 
-sudo bash <(curl -fsSL "$BASE_URL/01-rooted-remove-software.sh")
-sudo bash <(curl -fsSL "$BASE_URL/02-rooted-update-fedora.sh")
-sudo bash <(curl -fsSL "$BASE_URL/03-rooted-add-software.sh")
-bash <(curl -fsSL "$BASE_URL/04-non-rooted-add-software.sh")
+curl -fsSL "${BASE_URL}/01-rooted-remove-software.sh${BUST}" -o /tmp/01.sh
+curl -fsSL "${BASE_URL}/02-rooted-update-fedora.sh${BUST}"   -o /tmp/02.sh
+curl -fsSL "${BASE_URL}/03-rooted-add-software.sh${BUST}"    -o /tmp/03.sh
+curl -fsSL "${BASE_URL}/04-non-rooted-add-software.sh${BUST}" -o /tmp/04.sh
