5c7391020d4f8297bf3420a5fed10632af12aa57
matter172 <matter172@service.zaman.my>
2026-06-19 02:16:55 +0800

    Add files via upload

---
+++ 04-non-rooted-add-software.sh
@@ -0,0 +1,17 @@
+curl -f https://zed.dev/install.sh | sh
+echo 'export PATH=$HOME/.local/bin:$PATH' >> ~/.bashrc
+source ~/.bashrc
+
+pipx install gnome-extensions-cli --system-site-packages
+
+gext install tilingshell@ferrarodomenico.com
+
+flatpak install -y \
+	com.brave.Browser \
+	com.github.tchx84.Flatseal \
+	com.valvesoftware.Steam \
+	com.vysp3r.ProtonPlus \
+	com.heroicgameslauncher.hgl \
+	com.belmoussaoui.Decoder \
+	io.github.nozwock.Packet \
+	com.discordapp.Discord
