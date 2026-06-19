036846b19b45f677866f965d506a0abc7ff468e6
matter172 <matter172@service.zaman.my>
2026-06-19 15:23:11 +0800

    Refactor script to remove packages with checkbox output
    The script now removes GNOME bloat packages one at a time with checkbox output for user confirmation.

--- 01-rooted-remove-software.sh
+++ 01-rooted-remove-software.sh
@@ -1,24 +1,35 @@
-sudo dnf remove -y \
-	libreoffice-core.x86_64 \
-	gnome-tour.x86_64 \
-	firefox.x86_64 \
-	gnome-calendar.x86_64 \
-	gnome-boxes.x86_64 \
-	gnome-contacts.x86_64 \
-	mediawriter.x86_64 \
-	gnome-weather.noarch \
-	gnome-maps.x86_64 \
-	simple-scan.x86_64 \
-	yelp.x86_64 \
-	gnome-connections.x86_64 \
-	malcontent-control.x86_64 \
-	gnome-abrt.x86_64 \
-	gnome-font-viewer.x86_64 \
-	papers.x86_64 \
-	gnome-calculator.x86_64 \
-	gnome-clocks.x86_64 \
-	showtime.noarch \
-	gnome-characters.x86_64 \
-	loupe.x86_64 \
-	decibels.noarch \
-	gnome-system-monitor.x86_64
+#!/bin/bash
+# Removes default GNOME bloat, one package at a time, with checkbox output.
+
+LOG_FILE="${1:-/dev/null}"
+source "$(dirname "${BASH_SOURCE[0]}")/lib-checkbox.sh" || { echo "lib-checkbox.sh not found"; exit 1; }
+
+PACKAGES=(
+  "libreoffice-core.x86_64"
+  "gnome-tour.x86_64"
+  "firefox.x86_64"
+  "gnome-calendar.x86_64"
+  "gnome-boxes.x86_64"
+  "gnome-contacts.x86_64"
+  "mediawriter.x86_64"
+  "gnome-weather.noarch"
+  "gnome-maps.x86_64"
+  "simple-scan.x86_64"
+  "yelp.x86_64"
+  "gnome-connections.x86_64"
+  "malcontent-control.x86_64"
+  "gnome-abrt.x86_64"
+  "gnome-font-viewer.x86_64"
+  "papers.x86_64"
+  "gnome-calculator.x86_64"
+  "gnome-clocks.x86_64"
+  "showtime.noarch"
+  "gnome-characters.x86_64"
+  "loupe.x86_64"
+  "decibels.noarch"
+  "gnome-system-monitor.x86_64"
+)
+
+for pkg in "${PACKAGES[@]}"; do
+  checkbox "Remove ${pkg}" dnf remove -y "$pkg"
+done
