72228aa11f23d23a88bdd66a3f02f9174a1ed26d
matter172 <matter172@service.zaman.my>
2026-06-19 20:12:32 +0800

    Update Fedora upgrade script for latest stable version

--- upgrade-fedora.sh
+++ upgrade-fedora.sh
@@ -3,8 +3,8 @@
 # 1. Update everything and refresh repos
 sudo dnf upgrade --refresh -y
 
-# 2. Download the next version up
-sudo dnf system-upgrade download --releasever=$(( $(rpm -E %fedora) + 1 )) -y
+# 2. Download the next latest stable version
+sudo dnf system-upgrade download --releasever=$(curl -s https://getfedora.org/releases.json | jq -r '[.[].version | select(test("^\\d+$"))] | map(tonumber) | max')
 
 # 3. Trigger the reboot and install (Run this after the download finishes)
 sudo dnf system-upgrade reboot
