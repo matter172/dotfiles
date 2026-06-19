b3e47d3b3abbd756d53fd22c07f5c6f2e4afd817
matter172 <matter172@service.zaman.my>
2026-06-19 21:01:54 +0800

    Fix URL for downloading Fedora release version

--- upgrade-fedora.sh
+++ upgrade-fedora.sh
@@ -4,7 +4,7 @@
 sudo dnf upgrade --refresh -y
 
 # 2. Download the next latest stable version
-sudo dnf system-upgrade download --releasever=$(curl -s https://getfedora.org/releases.json | jq -r '[.[].version | select(test("^\\d+$"))] | map(tonumber) | max')
+sudo dnf system-upgrade download --releasever=$(curl -s https://fedoraproject.org/releases.json | jq -r '[.[].version | select(test("^\\d+$"))] | map(tonumber) | max')
 
 # 3. Trigger the reboot and install (Run this after the download finishes)
 sudo dnf system-upgrade reboot
